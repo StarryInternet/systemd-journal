294835241f397ad2de8e603d52703bddfe1c0913
Milo Turner <mturner@starry.com>
2020-10-12 19:43:06 -0400

    integration test that reads back from the journal

---
+++ tests/test_journal_send.rs
@@ -0,0 +1,14 @@
+extern crate sd_journal;
+
+static STRING_TO_LOG: &str = "Hello from sd_journal integration test";
+
+#[test]
+fn test_journal_send() {
+    sd_journal::send(STRING_TO_LOG).unwrap();
+
+    let mut cmd = std::process::Command::new("journalctl");
+    cmd.args(&["-r", "-n2", "--no-pager"]);
+    let stdout = cmd.output().unwrap().stdout;
+    let stdout = String::from_utf8_lossy(&stdout);
+    assert!(stdout.contains(STRING_TO_LOG), "{:?}", stdout);
+}
