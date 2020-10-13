0c1a5cd4186d3bd5d010d237bfde727ec79b9aaf
Milo Turner <mturner@starry.com>
2020-10-13 15:08:29 -0400

    add journal listing example in examples/

---
+++ examples/journal-list.rs
@@ -0,0 +1,18 @@
+use sd_journal::Journal;
+
+fn main() -> std::io::Result<()> {
+    let jrn = Journal::new();
+    let mut rdr = jrn.read()?;
+    let mut cnt = 0;
+    while let Some(mut ent) = rdr.next()? {
+        let msg = ent.get("MESSAGE")?.unwrap_or(b"(no message)");
+        let msg = String::from_utf8_lossy(msg);
+        println!("{:3}. {}", cnt + 1, msg);
+
+        cnt += 1;
+        if cnt >= 100 {
+            break;
+        }
+    }
+    Ok(())
+}
