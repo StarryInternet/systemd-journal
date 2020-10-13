3cdf02f672a2c3f290d5ded683e81644e98931b5
Milo Turner <mturner@starry.com>
2020-10-12 20:50:02 -0400

    modify integration test to include error!() as well as info!()

--- tests/test_journal_send.rs
+++ tests/test_journal_send.rs
@@ -1,15 +1,30 @@
 extern crate sd_journal;
 
-static STRING_TO_LOG: &str = "Hello from sd_journal integration test";
+use sd_journal::{error, info, Journal};
+
+static INFO_STRING: &str = "Hello from sd_journal integration test";
+static ERROR_STRING: &str = "Watch out, its sd_journal integration test!";
 
 #[test]
 fn test_journal_send() {
-    let jrn = sd_journal::Journal::new();
-    sd_journal::info!(jrn, "{}", STRING_TO_LOG).unwrap();
+    let jrn = Journal::new();
+    info!(jrn, "{}", INFO_STRING).unwrap();
 
     let mut cmd = std::process::Command::new("journalctl");
-    cmd.args(&["-r", "-n2", "--no-pager"]);
+    cmd.args(&["-r", "-n1", "-p6", "--no-pager"]);
     let stdout = cmd.output().unwrap().stdout;
     let stdout = String::from_utf8_lossy(&stdout);
-    assert!(stdout.contains(STRING_TO_LOG), "{:?}", stdout);
+    assert!(stdout.contains(INFO_STRING), "{:?}", stdout);
+}
+
+#[test]
+fn test_journal_error() {
+    let jrn = Journal::new();
+    error!(jrn, "{}", ERROR_STRING).unwrap();
+
+    let mut cmd = std::process::Command::new("journalctl");
+    cmd.args(&["-r", "-n1", "-p3", "--no-pager"]);
+    let stdout = cmd.output().unwrap().stdout;
+    let stdout = String::from_utf8_lossy(&stdout);
+    assert!(stdout.contains(ERROR_STRING), "{:?}", stdout);
 }
